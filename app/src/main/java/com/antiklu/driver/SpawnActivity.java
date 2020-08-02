338984232f83bda42b8c435ae007ace7058a99d7
ahlamls <fulldoang@gmail.com>
2020-08-03 06:05:10 +0700

    Initial commit

---
+++ app/src/main/java/com/antiklu/driver/SpawnActivity.java
@@ -0,0 +1,14 @@
+package com.antiklu.driver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpawnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spawn);
+    }
+}
